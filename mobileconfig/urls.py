f7b3546335a14f0d4de200ada9c4a63dfc966748
Tao Fei <Filia.Tao@gmail.com>
2015-04-20 15:13:16 +0000

    fix content type

--- mobileconfig/urls.py
+++ mobileconfig/urls.py
@@ -3,7 +3,6 @@ from mobileconfig.views import MobileConfigView
 
 urlpatterns = [
     url(r'^(?P<vpn_username>[^/]+)/(?P<server>[\w\.\-]+).mobileconfig$',
-        #MobileConfigView.as_view(content_type="application/octet-stream"), name='mobile_config'),
-        MobileConfigView.as_view(content_type="text/plain"), name='mobile_config'),
+        MobileConfigView.as_view(content_type="application/octet-stream"), name='mobile_config'),
 ]
 
